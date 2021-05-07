fab79d2f997ff2175ba4b33ff30a19a9466f38cc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-05-07 12:02:26 +0900

    try-pango: minimumize

---
+++ bugs/haddock/try-pango/src/Some.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Some where
+
+import Types
+
+some
--- bugs/haddock/try-pango/src/Types.hs
+++ bugs/haddock/try-pango/src/Types.hs
@@ -1,34 +1,13 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE PatternSynonyms, ViewPatterns #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Types where
 
 import Language.Haskell.TH
 import Foreign.Ptr
-import Foreign.C.String
-import Data.Int
-import System.IO.Unsafe
+import Foreign.C
 
-mkMemberGen :: Name -> Name -> String -> Integer -> DecsQ
-mkMemberGen t c n v = sequence [
-	patSynSigD (mkName n) (conT t),
-	patSynD (mkName n) (prefixPatSyn [])
-		(explBidir [clause [] (normalB (conE c `appE` litE (IntegerL v))) []])
-		(conP c [litP (IntegerL v)])
-	]
-
-newtype PangoScript = PangoScript Int32 deriving Show
-
-mkMemberPangoScript :: String -> Integer -> DecsQ
-mkMemberPangoScript = mkMemberGen ''PangoScript 'PangoScript
-
-newtype PangoLanguage = PangoLanguage_ (Ptr PangoLanguage)
-
-pangoLanguageFromString :: String -> PangoLanguage
-pangoLanguageFromString l = unsafePerformIO $ withCString l \cl ->
-	PangoLanguage_ <$> c_pango_language_from_string cl
+some :: DecsQ
+some = pure []
 
 foreign import ccall "pango_language_from_string"
-	c_pango_language_from_string :: CString -> IO (Ptr PangoLanguage)
+	c_pango_language_from_string :: CString -> IO (Ptr ())
